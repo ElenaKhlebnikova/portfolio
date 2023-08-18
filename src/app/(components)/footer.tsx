53d19f558b6b775a87197c3ba344e5b48dbf9871
ElenaKhlebnikova <elenano555@gmail.com>
2023-08-18 16:40:14 +0200

    started working

---
+++ src/app/(components)/footer.tsx
@@ -0,0 +1,17 @@
+'use client'
+import Image from 'next/image'
+
+const Footer = () => {
+    return (
+        <div className="m-0">
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
+                enim doloremque quae veritatis fugit animi nobis culpa hic
+                similique. Totam veniam corporis, error voluptates dicta velit
+                harum quasi ullam sint!
+            </p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/app/(components)/main-section.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Image from 'next/image'
+
+const MainSection = () => {
+    return (
+        <div className=" m-0 h-3/4  text-white font-display text-7xl font-bold flex items-center justify-end  mb-12">
+            <p className="w-1/2 mr-24 mb-12">Hi! I am a frontend developer.</p>
+        </div>
+    )
+}
+
+export default MainSection
